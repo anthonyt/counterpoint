77944ea9205d9fe63cdaff13542b89b4ee8b2009
Anthony Theocharis <anthony@simplestation.com>
2009-12-14 12:54:55 -0800

    Add some starter functions and notes for counterpoint evaluation

--- demo.py
+++ demo.py
@@ -6,6 +6,12 @@ from mingus.midi.MidiFileOut import write_Composition
 from mingus.containers import Note, NoteContainer, Bar, Composition, Instrument, Track
 from mingus.extra.LilyPond import from_Composition, to_png, to_pdf
 
+###
+# WE'RE GOING TO MAKE A MAJOR ASSUMPTION IN THIS PROGRAM, AND ASSUME THAT
+# NO TRACK WILL HAVE POLYPHONY. ALL NOTES IN A TRACK WILL START ONLY AFTER
+# THE PREVIOUS NOTE HAS FINISHED.
+###
+
 # Set up our vocal classes
 class Soprano(Instrument):
     name = u'Soprano'
@@ -154,5 +160,87 @@ MIDI_to_Composition(file):
   tuple with the last used tempo in beats per minute (this will change in the
   future). This function can raise all kinds of exceptions (IOError,
   HeaderError, TimeDivisionError, FormatError), so be sure to try and catch.
+"""
+
+
+def all_notes_line_up(cf_track, ctp_track):
+    """Counterpoint moves with same rhythm as cantus-firmus (note for note)
+
+    We check this by ensuring that no note in either track starts without
+    Ga corresponding note, with equal duration, in the other track.
+
+    Returns a tuple (num errors, unmatched notes in CF, unmatched notes in CTP)
+    """
+    def get_concurrent_bars(track_a, track_b):
+        if len(track_a.bars) != len(track_b.bars):
+            raise Exception('Tracks are not of the same length')
+
+        for i in range(0, len(track_a.bars)):
+            a = track_a.bars[i]
+            b = track_b.bars[i]
+            if a.meter != b.meter:
+                raise Exception("Bars at index %d don't have the same meter.")
+
+            yield a, b
+
+    def bar_has_matching_note(bar, start, dur):
+        for y in bar:
+            if y[0] == start and y[1] == dur:
+                return True
+        return False
+
+    def find_unmatched_notes(bar_a, bar_b):
+        unmatched = []
+        for x in bar_a:
+            start = x[0]
+            dur = x[1]
+            if not bar_has_matching_note(bar_b, start, dur):
+                unmatched.append(x)
+
+        return unmatched
+
+    cf_unmatched = []
+    ctp_unmatched = []
+    errors = 0
+    for cf_bar, ctp_bar in get_concurrent_bars(cf_track, ctp_track):
+        un = find_unmatched_notes(cf_bar, ctp_bar)
+        errors += len(un)
+        cf_unmatched.append(un)
+
+        un = find_unmatched_notes(ctp_bar, cf_bar)
+        errors += len(un)
+        ctp_unmatched.append(un)
+
+    return errors, cf_unmatched, ctp_unmatched
+
+a, b, c = all_notes_line_up(cf_track, ctp_track)
+print a, 'unmatched notes'
+print b
+print c
+
+string = from_Composition(myComp)
+print string
+to_png(string, 'demo.png')
+
+
+
+"""
+Methods we need on each note:
+    what is your time signature
+    what is your starting beat number
+    what is your duration
+    what is your key signature
+    what is your scale degree
+    what is the interval between you and this other note
+
+    get previous note
+    get next note
+        find next local minimum
+        find next local maximum
+
+    we can do this with a
+    doubly linked list, with properties like note, midi note number, bar number, beat number, duration, and a reference to the enclosing data structure.
+    linked list has accessor functions to get note wrappers by bar/beat number.
+
 """
 
