4a584cdc2c8d64189a8bba8fa1bc76673fb96d01
Anthony Theocharis <anthony@simplestation.com>
2009-12-14 18:49:16 -0800

    Simplify logic in 'matching rhythms' rule

--- rules.py
+++ rules.py
@@ -4,32 +4,24 @@ def all_notes_line_up(cf_list, ctp_list):
     """Counterpoint moves with same rhythm as cantus-firmus (note for note)
 
     We check this by ensuring that no note in either track starts without
-    Ga corresponding note, with equal duration, in the other track.
+    A corresponding note, with equal duration, in the other track.
 
     Returns a tuple (num errors, unmatched notes in CF, unmatched notes in CTP)
+
+    Note: this function's worst-case run-time could be made much more efficient
     """
-    def has_matching_note(note, list, offset):
-        for x in list[offset:]:
-            if x.bar > note.bar:
-                return False, offset
-            offset += 1
-            if x.beat == note.beat \
-            and x.duration == note.duration:
-                return True, offset
-        return False, offset
+    a_list = [x for x in cf_list]  # cast NoteList to list
+    b_list = [x for x in ctp_list] # cast NoteList to list
 
-    def a_n_l_u(a_list, b_list):
-        unmatched = []
-        i = 0
-        for a_note in a_list:
-            has_match, i = has_matching_note(a_note, b_list, i)
-            if not has_match:
-                errors += 1
-                unmatched.append(a_note)
-        return unmatched
+    # remove matched notes
+    for a_note in [x for x in a_list]:
+        for b_note in [x for x in b_list]:
+            if (a_note.start, a_note.end) == (b_note.start, b_note.end):
+                # remove the matched pair from their respective lists
+                a_list.remove(a_note)
+                b_list.remove(b_note)
+                break
+    errors = len(a_list) + len(b_list) # count up the unmatched notes
 
-    unmatched_cf = a_n_l_u(cf_list, ctp_list)
-    unmatched_ctp = a_n_l_u(ctp_list, cf_list)
-    errors = len(unmatched_cf) + len(unmatched_ctp)
+    return errors, a_list, b_list
 
-    return errors, unmatched_cf, unmatched_ctp
